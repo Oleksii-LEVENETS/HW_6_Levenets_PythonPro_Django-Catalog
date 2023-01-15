ff922c9b70a1d449c7cc9d91ae6b1bd9c283efbe
Oleksii LEVENETS <oleksii.levenets@icloud.com>
2023-01-15 19:25:24 +0200

    added Flake8 and retaled to it changes

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'catalog',
 ]
 
