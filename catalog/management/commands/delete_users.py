8a8a94bc3dd67105b3e72d5e5c61d7ec37200b8d
Oleksii LEVENETS <oleksii.levenets@icloud.com>
2023-01-27 12:29:10 +0200

    after FB hw_8: complicated delete

--- catalog/management/commands/delete_users.py
+++ catalog/management/commands/delete_users.py
@@ -17,8 +17,7 @@ https://simpleisbetterthancomplex.com/tutorial/2018/08/27/how-to-create-custom-d
 """
 
 from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand
-from django.template.defaultfilters import pluralize
+from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
@@ -31,22 +30,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         list_delete = options['user_id']
-        list_superusers = User.objects.filter(is_superuser=True).values_list('id', flat=True)
-        both = set(list_superusers) & set(list_delete)
+        super_users = User.objects.filter(id__in=list_delete, is_superuser=True)
+        if super_users.exists():
+            raise CommandError('Can not delete admin user!')
 
-        if len(both) > 0:
-            self.stderr.write(f"Superuser{pluralize(both)} with id {list(both)} cannot be deleted!")
-            exit()
-
-        d = User.objects.filter(id__in=list_delete)
-        d_id = list(User.objects.filter(id__in=list_delete).values_list('id', flat=True))
-
-        if d.exists():
-            d.delete()
-            if not d.exists():
-                self.stdout.write(self.style.SUCCESS(f"Successfully deleted user{pluralize(d_id)} "
-                                                     f"with id: {d_id}"))
-            else:
-                self.stdout.write(self.style.ERROR('Oops.. something went wrong.'))
-        else:
-            self.stderr.write(f"User{pluralize(list_delete)} with id {list_delete} do not exist in DataBase.")
+        User.objects.filter(id__in=list_delete).delete()
+        self.stdout.write(self.style.SUCCESS("Users successfully deleted."))
